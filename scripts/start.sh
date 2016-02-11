681cc3c12d7dcb44facc9c468a3fb10eef9a4c9f
Anton Gulenko <anton.gulenko@googlemail.com>
2016-02-11 13:02:33 +0000

    better start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source "$HOME/.bash_localrc"
 while IFS='' read -r line || [[ -n "$line" ]]; do
+    test -z "$line" && continue
     arr=($line)
     program=${arr[0]}
     if [ "$program" = "sudo" ]; then
